2d73df750c06964a25438cd51e5e7b23420690ec
harshagrl <harshkr.agrl@gmail.com>
2026-07-02 17:36:42 +0530

    Complete Design Patterns Exercise 1 - Singleton Pattern

---
+++ week-1/Design principles & Patterns/Exercise1_SingletonPattern/Logger.java
@@ -0,0 +1,21 @@
+public class Logger {
+
+  private static Logger instance;
+
+  private Logger() {
+    System.out.println("Logger Object Created");
+  }
+
+  public static Logger getInstance() {
+
+    if (instance == null) {
+      instance = new Logger();
+    }
+
+    return instance;
+  }
+
+  public void log(String message) {
+    System.out.println("Log: " + message);
+  }
+}
